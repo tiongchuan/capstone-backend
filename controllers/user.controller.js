9642b7da995f91687cff51ccc6fc2a8181a721ae
MiaKim0804 <kimmichino0804@gmail.com>
2022-09-14 12:02:43 +0800

    move: multer to global

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -8,6 +8,41 @@ import {
 import multer from "multer"
 import path from "path"
 
+// set storage engine
+const storage = multer.diskStorage({
+  destination: function(req, file, cb){
+    cb(null, 'Images');
+  },
+  filename: function(req, file, cb){
+    cb(null, Date.now() + path.extname(file.originalname));
+  }
+});
+
+// init upload
+const upload = multer({
+  storage: storage,
+  limits: {fileSize: 2000000}, // 2MB limit for image size 
+  fileFilter: function(req, file, cb){
+    checkFileType(file, cb);
+  }
+}).single('profile_img');
+
+// check file type
+function checkFileType(file, cb){
+  // allowed ext
+  const filetypes = /jpeg|jpg|png|gif/;
+  // check ext
+  const extname = filetypes.test(path.extname(file.originalname).toLowerCase());
+  // check mime
+  const mimetype = filetypes.test(file.mimetype);
+
+  if(mimetype && extname){
+    return cb(null, true);
+  } else {
+    cb('Error: Images Only!');
+  }
+}
+
 class UserController {
 
     // GET /general/user/:userId
@@ -40,37 +75,15 @@ class UserController {
     // Upload profile_img to server 
     async updateProfile_img(req, res, next) {
       try {
-        const storage = multer.diskStorage({
-            destination: function (req, file, cb) {
-                cb(null, 'Images')
-            },
-            filename: function (req, file, cb) {
-                cb(null, Date.now() + path.extname(file.originalname));
-            }
-        });
-        const upload = multer({ 
-          storage: storage,
-          limits: { fileSize: 2000000 },
-          fileFilter: function (req, file, cb) {
-            const filetypes = /jpeg|jpg|png|gif/
-            const mimetype = filetypes.test(file.mimetype)
-            const extname = filetypes.test(path.extname(file.originalname).toLowerCase())
-            if (mimetype && extname) {
-              return cb(null, true)
-            }
-            cb("Error: File upload only supports the following filetypes - " + filetypes)
-          }, 
-        }).single("profile_img");
-
-        // POST /protected/user/updateProfile_img/:userId
-        upload(req, res, async function (err) {
-          if (err) {
-            return res.status(400).json({ message: err });
-          }
-          const result = await updateProfileImage(req.params.userId, req.file.filename);
-          res.status(result.status);
-          return res.json({ data: result.data, message: result.message });
-        });
+      // Upload profile_img to server
+      upload(req, res, async function (err) {
+        if (err) {
+          return res.status(400).json({ message: err });
+        }
+        const result = await updateProfileImage(req.params.userId, req.file.filename);
+        res.status(result.status);
+        return res.json({ data: result.data, message: result.message });
+      });
       } catch (err) {
         return res.status(500).json({ message: err.message });
       }
@@ -87,7 +100,6 @@ class UserController {
       }
     }
   
-
     // // GET /general/tutors
     // async retrieveTutors(req, res, next) {
   
